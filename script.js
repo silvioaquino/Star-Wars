eff610c0ee33d554d44993e34ce81ecb06b205c8
Silvio Aquino <silvios.aquino@gmail.com>
2023-08-15 13:38:49 -0300

    seven commit

--- script.js
+++ script.js
@@ -57,3 +57,33 @@ async function loadCharacters(url) {
         console.log(error)
     }
 }
+
+async function loadNextPage() {
+    if (!currentPageUrl) return;
+
+    try {
+        const response = await fetch(currentPageUrl)
+        const responseJson = await response.json()
+
+        await loadCharacters(responseJson.next)
+
+    } catch (error) {
+        console.log(error)
+        alert('Erro ao carregar a próxima página')
+    }
+} 
+
+async function loadPreviousPage() {
+    if (!currentPageUrl) return;
+
+    try {
+        const response = await fetch(currentPageUrl)
+        const responseJson = await response.json()
+
+        await loadCharacters(responseJson.previous)
+
+    } catch (error) {
+        console.log(error)
+        alert('Erro ao carregar a página anterior')
+    }
+} 
